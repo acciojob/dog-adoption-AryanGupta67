aa0a0ee960098472922be7d90e254c5312b52c1f
Aryan Gupta <124511705+AryanGupta67@users.noreply.github.com>
2025-07-05 06:49:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-// Optional: add hover alert or click tracking
-document.querySelectorAll("a").forEach((link) => {
-  link.addEventListener("click", () => {
-    console.log("Image link clicked: " + link.href);
-  });
-});
+
 
